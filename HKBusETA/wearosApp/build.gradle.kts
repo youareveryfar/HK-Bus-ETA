1b1e90ba1445571e01b5a8a92262fd74e3b48ee8
LOOHP <jamesloohp@gmail.com>
2026-05-13 08:50:23 +0100

    Fixed scrolling divide by zero

--- HKBusETA/wearosApp/build.gradle.kts
+++ HKBusETA/wearosApp/build.gradle.kts
@@ -35,8 +35,8 @@ android {
         applicationId = "com.loohp.hkbuseta"
         minSdk = 30
         targetSdk = 36
-        versionCode = 1245
-        versionName = "2.6.14"
+        versionCode = 1247
+        versionName = "2.6.15"
         vectorDrawables {
             useSupportLibrary = true
         }
